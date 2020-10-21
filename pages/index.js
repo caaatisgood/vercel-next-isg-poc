2d3c5e7a1ed408a9d1d20ed1b9589b46a4aba6a6
robin <almigod0718@gmail.com>
2020-10-21 17:12:17 +0800

    Tweak foo variable declaration

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-const FOO = process.env.FOO
+const FOO = process.env.NEXT_PUBLIC_FOO
 
 export default function Home() {
   console.log(process.env)
